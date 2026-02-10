27a3e99f860d0dde0c60efb7e2721ec20f4c9dc2
Juanjoo24 <github.goatskin743@passinbox.com>
2026-02-10 16:26:53 +0100

    actualizando base datos

--- tienda_zapas.sql
+++ tienda_zapas.sql
@@ -1,10 +1,74 @@
-CREATE DATABASE IF NOT EXISTS tienda_zapas;
-USE tienda_zapas;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-02-2026 a las 16:25:45
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-CREATE TABLE zapatillas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    marca VARCHAR(255),
-    modelo VARCHAR(255),
-    talla INT,
-    precio DOUBLE
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda_zapas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `zapatillas`
+--
+
+CREATE TABLE `zapatillas` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(255) DEFAULT NULL,
+  `modelo` varchar(255) DEFAULT NULL,
+  `talla` int(11) DEFAULT NULL,
+  `precio` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `zapatillas`
+--
+
+INSERT INTO `zapatillas` (`id`, `marca`, `modelo`, `talla`, `precio`) VALUES
+(1, 'NIKE', 'Jordan 1', 32, 200),
+(2, 'Nike', 'Air Jordan 1 Low', 42, 129.99),
+(3, 'Adidas', 'Ultraboost Light', 44, 180),
+(4, 'New Balance', '550 White Grey', 41, 140),
+(5, 'Vans', 'Old Skool Classic', 39, 75.5),
+(6, 'Converse', 'Chuck Taylor All Star', 40, 70),
+(7, 'Nike', 'Dunk Low Panda', 43, 110);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `zapatillas`
+--
+ALTER TABLE `zapatillas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `zapatillas`
+--
+ALTER TABLE `zapatillas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
